eb518e066316a79f04de542511727d4827bd768a
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2023-02-06 08:45:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const express = require('express');
+var cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+    /*
+    req.body.items
+    [
+        {
+            id: 1,
+            quantity: 3
+        }
+    ]
+    stripe wants
+    [
+        {
+            price: 1,
+            quantity: 3
+        }
+    ]
+    */
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
